c4c41d84924b16588b707e26de1906d7043b98f7
Matthew Moe <38055340+4Empyre@users.noreply.github.com>
2018-06-07 03:00:42 -0700

    Add files via upload
    First JavaScript project

---
+++ 06javascript_declare/declare.js
@@ -0,0 +1,17 @@
+var firstcar = 1988;
+var secondcar = 1994;
+var thirdcar = 1985;
+var fourthcar = 2004;
+
+var string1 = "My first car gave me inspiration.";
+var string2 = "My second car gave me satisfaction.";
+var string3 = "My fourth car was a dream come true.";
+
+var turbo = true;
+var hp = true;
+
+var und = "";
+
+console.log("My 1st:", string1 + " It was produced in " + firstcar + ".");
+console.log("My GSR:", string2 + " It was produced in " + secondcar + ".");
+console.log("My STI:", string3 + " It was produced in " + fourthcar + ", has a turbo " + turbo + ", and has over 300hp " + hp);
