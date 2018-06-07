bf60d575baca109a456a1f150d931271edcfc1a5
Matthew Moe <38055340+4Empyre@users.noreply.github.com>
2018-06-07 03:03:51 -0700

    Add files via upload

---
+++ 14javascript_numbersonly/numbersonly.js
@@ -0,0 +1,15 @@
+function numbersOnly(arr)
+{
+  var newArray = [];
+  for (var i = 0; i < arr.length; i++)
+  {
+    if (typeof arr[i] === "number")
+    {
+      newArray.push(arr[i])
+    }
+  }
+  //console.log(newArray); for debugging
+  return newArray;
+}
+var test = numbersOnly([1, 2, "test", 3, 4, "fuuuck"]);
+console.log(test);
